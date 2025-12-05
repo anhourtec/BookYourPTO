5e538aded7ec3c919ae22ca755e9a89af2d28077
nishchayjeet <nishchayjeets@gmail.com>
2025-12-04 18:48:21 -0800

    Install tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui'],
-  
+ modules: [
+    '@nuxt/ui',
+    '@vueuse/motion/nuxt'
+  ],
+    
   colorMode: {
     preference: 'light', // default value
     fallback: 'light', // fallback value
@@ -12,5 +18,12 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
 })
