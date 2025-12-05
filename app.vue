ffe1ff051cceaf58de187b96717070503d22beeb
nishchayjeet <nishchayjeets@gmail.com>
2025-12-05 13:00:12 -0800

    Fix Nuxt Layout Error , and removed default layout.vue

--- app.vue
+++ app.vue
@@ -2,17 +2,17 @@
   <div class="min-h-screen flex flex-col bg-white dark:bg-gray-950">
     <Header />
     <main class="flex-1">
-      <NuxtPage />
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
     </main>
+    <Footer />
   </div>
-  <Footer />
-
 </template>
 
 <script setup lang="ts">
 const colorMode = useColorMode()
 
-// Force light mode on mount if not set
 onMounted(() => {
   if (!localStorage.getItem('nuxt-color-mode')) {
     colorMode.preference = 'light'
